bfe446764869e9fcf4039cc000fb0fead3db2b36
Hubert <hdudowicz@gmail.com>
2018-07-06 13:40:34 +0100

    Script finished

--- Modern And Traditional/ModernTraditional.py
+++ Modern And Traditional/ModernTraditional.py
@@ -3,6 +3,7 @@ import pypyodbc as pyodbc
 import re
 import pprint
 import numpy
+from collections import Counter
 
 db_host = 'DESIGNER1\INTRANET'
 db_name = 'intranet'
@@ -13,18 +14,14 @@ db = pyodbc.connect(connection_string)
 cursor = db.cursor()
 pp = pprint.PrettyPrinter(indent=4)
 
-#Getting max parentid
-cursor.execute('SELECT max(parentid) from prod_parent')
-
-maxid = 500
-
-cursor.execute('SELECT * from prod_parent')
+cursor.execute('SELECT * from prod_parent_test')
 
 columns = [column[0] for column in cursor.description]
 
 parentids = []
 
-parentMTs = []
+parentMTs = [[], []]
+
 def checkEqual(iterator):
     iterator = iter(iterator)
     try:
@@ -32,6 +29,12 @@ def checkEqual(iterator):
     except StopIteration:
         return True
     return all(first == rest for rest in iterator)
+
+def majority_element(lst):
+    data = Counter(lst)
+    return max(lst, key=data.get)
+
+
 
 for row in cursor.fetchall():
     parentids.append(row[0])
@@ -50,22 +53,31 @@ for id in parentids:
     # Iterate over child rows, add M or T to parentMTs if all modern_traditional children fields are equal or 0 if not equal
     for childrow in childrows:
         if childrows.count(childrow) == len(childrows):
-            parentMTs.append(0)
+            parentMTs[0].append(id)
+            parentMTs[1].append(majority_element(childrows)[24])
             items += 1
             break
         else:
-            parentMTs.append(childrow[24])
+            parentMTs[0].append(id)
+            parentMTs[1].append(childrow[24])
             items += 1
             break
-    # Iterate up until id 1000
-    if id >= 1000:
-        break
+
+    # # Iterate up until id 1000
+    # if id >= 100:
+    #     break
+
+i = 0
+for id in parentMTs[0]:
+    # print('UPDATE prod_parent_test SET modern_traditional = \'' + parentMTs[1][i] + '\' WHERE parentid = '+str(id))
+    if parentMTs[1][i] != None:
+        cursor.execute('UPDATE prod_parent_test SET modern_traditional = \'' + parentMTs[1][i] + '\' WHERE parentid = '+str(id))
+    i += 1
+cursor.commit()
 
 print(parentMTs)
 print(len(parentMTs))
 print('Items: ' + str(items))
-
-
 
 cursor.close()
 db.close()
